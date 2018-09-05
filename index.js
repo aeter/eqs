b8b15a59bf1992d3934b0babb0f7f239e6b57f94
A. Nackov <anackov@gmail.com>
2018-09-05 23:46:24 +0300

    some js refactoring

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
 var EQS = {};
 
-EQS.main = function() {
-    var DAMAGE_DESCRIPTIONS = {
-        "0": "UNKNOWN",
-        "1": "LIMITED (<1m USD)",
-        "2": "MODERATE (1-5m USD)",
-        "3": "SEVERE (5-524m USD)",
-        "4": "EXTREME (>525m USD)"
-    }
+EQS.DAMAGE_DESCRIPTIONS = {
+    "0": "UNKNOWN",
+    "1": "LIMITED (<1m USD)",
+    "2": "MODERATE (1-5m USD)",
+    "3": "SEVERE (5-524m USD)",
+    "4": "EXTREME (>525m USD)"
+};
 
-    class DB {
-        static init() {
-            alasql.promise(
-                ["CREATE TABLE IF NOT EXISTS quakes",
-                 "SELECT * INTO quakes from TSV('data/significant_quakes.tsv')",
-                ]).then(function(data) { 
-                    // for cleaner js code (sliders use 0, not NULL), we update the DB -
-                    // we insert '0's where there's NULL for some DB columns.
-                    alasql("UPDATE quakes SET DAMAGE_DESCRIPTION = 0 WHERE DAMAGE_DESCRIPTION = ''") 
-                    alasql("UPDATE quakes SET DEATHS = 0 WHERE DEATHS = ''") 
-                    alasql("UPDATE quakes SET TOTAL_DEATHS = 0 WHERE TOTAL_DEATHS = ''") 
-                });
+EQS.ZOOM_SIZE = 5;
+
+EQS.main = function() {
+    EQS.init_db();
+    EQS.init_world_map();
+    EQS.create_year_slider();
+    EQS.create_damage_slider();
+    EQS.create_deaths_info_slider();
+    EQS.setup_world_map_zoom();
+}
+
+// workaround for https://github.com/neveldo/jQuery-Mapael/issues/253
+// shrinks the bubbles when zooming the map in, expands them on zoom-out.
+EQS.setup_world_map_zoom = function() {
+    function zoom() {
+        var zoom_level = $(".mapcontainer").data("mapael").zoomData.zoomLevel;
+        var new_zoom_size = zoom_level <= 10 ? 5 : 1;
+        if (new_zoom_size != EQS.ZOOM_SIZE) {
+            EQS.ZOOM_SIZE = new_zoom_size;
+            EQS.recalculate_quakes();
         }
     }
-    DB.init();
+    $(".mapcontainer").on("zoom.mapael", zoom);
+}
 
+EQS.init_db = function() {
+    alasql.promise(
+        ["CREATE TABLE IF NOT EXISTS quakes",
+         "SELECT * INTO quakes from TSV('data/significant_quakes.tsv')",
+        ]).then(function(data) { 
+            // for cleaner js code (sliders use 0, not NULL), we update the DB -
+            // we insert '0's where there's NULL for some DB columns.
+            alasql("UPDATE quakes SET DAMAGE_DESCRIPTION = 0 WHERE DAMAGE_DESCRIPTION = ''") 
+            alasql("UPDATE quakes SET DEATHS = 0 WHERE DEATHS = ''") 
+            alasql("UPDATE quakes SET TOTAL_DEATHS = 0 WHERE TOTAL_DEATHS = ''") 
+        });
+};
+
+EQS.init_world_map = function() {
     var maparea = $(".mapcontainer");
     maparea.mapael({
         map: {
@@ -48,8 +70,12 @@ EQS.main = function() {
             }
         }
     });
+};
+
+EQS.create_year_slider = function() {
     noUiSlider.create($('#year_slider').get(0), {
-        start: [-10, 9],
+        //start: [-10, 9],
+        start: [0, 100],
         step: 1,
         connect: true,
         tooltips: true,
@@ -66,7 +92,10 @@ EQS.main = function() {
             density: 1000
         }
     });
+    year_slider.noUiSlider.on('end', EQS.recalculate_quakes);
+}
 
+EQS.create_damage_slider = function() {
     noUiSlider.create($('#damage_slider').get(0), {
         start: [0, 4],
         step: 1,
@@ -77,10 +106,13 @@ EQS.main = function() {
         },
         pips: {
             mode: 'steps',
-            format: {to: function(value, type) { return DAMAGE_DESCRIPTIONS[value]; } }
+            format: {to: function(value, type) { return EQS.DAMAGE_DESCRIPTIONS[value]; } }
         }
     });
+    damage_slider.noUiSlider.on('end', EQS.recalculate_quakes);
+}
 
+EQS.create_deaths_info_slider = function() {
     noUiSlider.create($('#deaths_info_slider').get(0), {
         start: [0, 830000],
         step: 1,
@@ -99,145 +131,130 @@ EQS.main = function() {
             density: 1000
         }
     });
-
-    damage_slider.noUiSlider.on('end', recalculate_quakes);
-    year_slider.noUiSlider.on('end', recalculate_quakes);
-    deaths_info_slider.noUiSlider.on('end', recalculate_quakes);
-
-    class QuakeToolTip {
-        static magnitude(quake) {
-            if (quake['EQ_PRIMARY'] != '') 
-                return 'Magnitude: ' + quake['EQ_PRIMARY'] + '<br>';
-            else
-                return '';
-        }
-
-        static damage_description(quake) {
-            if (quake['DAMAGE_DESCRIPTION'] == "4")
-                return '<span style="color:red;">Damage: ' + DAMAGE_DESCRIPTIONS[quake['DAMAGE_DESCRIPTION']] + '</span><br>';
-            else
-                return 'Damage: ' + DAMAGE_DESCRIPTIONS[quake['DAMAGE_DESCRIPTION']] + '<br>';
-        }
-
-        static date(quake) {
-            if (quake['YEAR'] != "" && quake['MONTH'] != '' && quake["DAY"] != '')
-                return "Date: " + quake['YEAR'] + "-" + quake["MONTH"] + "-" + quake["DAY"] + '<br>';
-            else
-                return "Date: " + quake['YEAR'] + '<br>';
-        }
-
-        static deaths(quake) {
-            var deaths = quake['TOTAL_DEATHS'] == '0' ? parseInt(quake['DEATHS']) : parseInt(quake['TOTAL_DEATHS']);
-            
-            if (deaths > 10000)  {
-                return '<span style="color:red;">Total deaths: ' + deaths + '</span>';
-            } else {
-                deaths = deaths == 0 ? "-" : deaths; 
-                return 'Total deaths: ' + deaths;
-            }
-        }
-
-        static mmi_intensity(quake) {
-            return quake['INTENSITY'] == '' ? '' : 'Modified Mercalii Intensity: ' + quake['INTENSITY'] + "<br>";
-        }
-
-        static tsunami(quake) {
-            return quake['FLAG_TSUNAMI'] == '' ? '' : 'Tsunami: yes<br>';
-        }
-
-        static focal_depth(quake) {
-            return quake['FOCAL_DEPTH'] == '' ? '' : 'Focal depth: ' + quake['FOCAL_DEPTH'] + ' km.<br>';
-        }
-
-        static injuries(quake) {
-            if (quake['TOTAL_INJURIES'] != '')
-                var injuries = parseInt(quake['TOTAL_INJURIES']);
-            else if (quake['INJURIES'] != '')
-                var injuries = parseInt(quake['INJURIES']);
-            else
-                var injuries = '';
-
-            return injuries == '' ? '' : 'Total injuries: ' + injuries + '<br>';
-        }
-
-        static houses_destroyed(quake) {
-            if (quake['TOTAL_HOUSES_DESTROYED'] != '')
-                var houses_destroyed = parseInt(quake['TOTAL_HOUSES_DESTROYED']);
-            else if (quake['HOUSES_DESTROYED'] != '')
-                var houses_destroyed = parseInt(quake['HOUSES_DESTROYED']);
-            else
-                var houses_destroyed = '';
-
-            return houses_destroyed == '' ? '' : 'Total houses destroyed: ' + houses_destroyed + '<br>';
-        }
-
-        static html(quake) {
-            return QuakeToolTip.date(quake)
-                + "Location: " + quake['LOCATION_NAME'] + '<br>'
-                + QuakeToolTip.magnitude(quake)
-                + QuakeToolTip.damage_description(quake)
-                + QuakeToolTip.mmi_intensity(quake)
-                + QuakeToolTip.focal_depth(quake)
-                + QuakeToolTip.tsunami(quake)
-                + QuakeToolTip.houses_destroyed(quake)
-                + QuakeToolTip.injuries(quake)
-                + QuakeToolTip.deaths(quake);
-        }
-    }
-
-    // workaround for https://github.com/neveldo/jQuery-Mapael/issues/253
-    // shrinks the bubbles when zooming the map in, expands them on zoom-out.
-    var zoom_size = 5;
-    class BubblesZoom {
-        static zoom() {
-            var zoom_level = maparea.data("mapael").zoomData.zoomLevel;
-            var new_zoom_size = zoom_level <= 10 ? 5 : 1;
-            if (new_zoom_size != zoom_size) {
-                zoom_size = new_zoom_size;
-                recalculate_quakes();
-            }
-        }
-    }
-    maparea.on("zoom.mapael", BubblesZoom.zoom);
-
-    function recalculate_quakes() {
-        var years = year_slider.noUiSlider.get();
-        var damage_descriptions = damage_slider.noUiSlider.get();
-        var deaths = deaths_info_slider.noUiSlider.get();
-
-        var query = 'select * from quakes where' 
-                    + ' YEAR >= __start_year__ and YEAR <= __end_year__ '
-                    + ' AND DAMAGE_DESCRIPTION >= __damage_min__ AND DAMAGE_DESCRIPTION <= __damage_max__ '
-                    + ' AND ((TOTAL_DEATHS >= __deaths_min__ AND TOTAL_DEATHS <= __deaths_max__) '
-                    + '   OR (DEATHS >= __deaths_min__  AND DEATHS <= __deaths_max__)) ';
-
-        var quakes = alasql(query
-                    .replace('__start_year__', years[0])
-                    .replace('__end_year__', years[1])
-                    .replace('__damage_min__', damage_descriptions[0])
-                    .replace('__damage_max__', damage_descriptions[1])
-                    .replace('__deaths_min__', deaths[0])
-                    .replace('__deaths_max__', deaths[1]));
-
-        var new_plots = {};
-        for (i = 0; i < quakes.length; i++) {
-          quakes[i]['latitude'] = quakes[i]['LATITUDE'];
-          quakes[i]['longitude'] = quakes[i]['LONGITUDE'];
-          quakes[i]['tooltip'] = {content: QuakeToolTip.html(quakes[i])};
-          quakes[i]['size'] = zoom_size;
-          if (quakes[i]['DAMAGE_DESCRIPTION'] == "4" 
-                  || parseInt(quakes[i]['TOTAL_DEATHS']) > 10000
-                  || parseInt(quakes[i]['DEATHS']) > 10000) {
-              quakes[i]['attrs'] = { fill: 'red' };
-          }
-          new_plots[quakes[i]['I_D']] = quakes[i];
-        }
-
-        $(".mapcontainer").trigger('update', [{
-            mapOptions: {'areas': {}, 'plots': {}}, 
-            newPlots: new_plots,
-            deletePlotKeys: Object.keys(maparea.data('mapael').plots),
-            animDuration: 0 
-        }]);
-    };
+    deaths_info_slider.noUiSlider.on('end', EQS.recalculate_quakes);
 }
+
+EQS.make_html_tooltip = function(quake) {
+    function magnitude(quake) {
+        if (quake['EQ_PRIMARY'] != '') 
+            return 'Magnitude: ' + quake['EQ_PRIMARY'] + '<br>';
+        else
+            return '';
+    }
+
+    function damage_description(quake) {
+        if (quake['DAMAGE_DESCRIPTION'] == "4")
+            return '<span style="color:red;">Damage: ' + EQS.DAMAGE_DESCRIPTIONS[quake['DAMAGE_DESCRIPTION']] + '</span><br>';
+        else
+            return 'Damage: ' + EQS.DAMAGE_DESCRIPTIONS[quake['DAMAGE_DESCRIPTION']] + '<br>';
+    }
+
+    function date(quake) {
+        if (quake['YEAR'] != "" && quake['MONTH'] != '' && quake["DAY"] != '')
+            return "Date: " + quake['YEAR'] + "-" + quake["MONTH"] + "-" + quake["DAY"] + '<br>';
+        else
+            return "Date: " + quake['YEAR'] + '<br>';
+    }
+
+    function deaths(quake) {
+        var deaths = quake['TOTAL_DEATHS'] == '0' ? parseInt(quake['DEATHS']) : parseInt(quake['TOTAL_DEATHS']);
+        
+        if (deaths > 10000)  {
+            return '<span style="color:red;">Total deaths: ' + deaths + '</span>';
+        } else {
+            deaths = deaths == 0 ? "-" : deaths; 
+            return 'Total deaths: ' + deaths;
+        }
+    }
+
+    function mmi_intensity(quake) {
+        return quake['INTENSITY'] == '' ? '' : 'Modified Mercalii Intensity: ' + quake['INTENSITY'] + "<br>";
+    }
+
+    function tsunami(quake) {
+        return quake['FLAG_TSUNAMI'] == '' ? '' : 'Tsunami: yes<br>';
+    }
+
+    function focal_depth(quake) {
+        return quake['FOCAL_DEPTH'] == '' ? '' : 'Focal depth: ' + quake['FOCAL_DEPTH'] + ' km.<br>';
+    }
+
+    function injuries(quake) {
+        if (quake['TOTAL_INJURIES'] != '')
+            var injuries = parseInt(quake['TOTAL_INJURIES']);
+        else if (quake['INJURIES'] != '')
+            var injuries = parseInt(quake['INJURIES']);
+        else
+            var injuries = '';
+
+        return injuries == '' ? '' : 'Total injuries: ' + injuries + '<br>';
+    }
+
+    function houses_destroyed(quake) {
+        if (quake['TOTAL_HOUSES_DESTROYED'] != '')
+            var houses_destroyed = parseInt(quake['TOTAL_HOUSES_DESTROYED']);
+        else if (quake['HOUSES_DESTROYED'] != '')
+            var houses_destroyed = parseInt(quake['HOUSES_DESTROYED']);
+        else
+            var houses_destroyed = '';
+
+        return houses_destroyed == '' ? '' : 'Total houses destroyed: ' + houses_destroyed + '<br>';
+    }
+
+    function html(quake) {
+        return date(quake)
+            + "Location: " + quake['LOCATION_NAME'] + '<br>'
+            + magnitude(quake)
+            + damage_description(quake)
+            + mmi_intensity(quake)
+            + focal_depth(quake)
+            + tsunami(quake)
+            + houses_destroyed(quake)
+            + injuries(quake)
+            + deaths(quake);
+    }
+
+    return html(quake);
+}
+
+EQS.recalculate_quakes = function() {
+    var years = year_slider.noUiSlider.get();
+    var damage_descriptions = damage_slider.noUiSlider.get();
+    var deaths = deaths_info_slider.noUiSlider.get();
+
+    var query = 'select * from quakes where' 
+                + ' YEAR >= __start_year__ and YEAR <= __end_year__ '
+                + ' AND DAMAGE_DESCRIPTION >= __damage_min__ AND DAMAGE_DESCRIPTION <= __damage_max__ '
+                + ' AND ((TOTAL_DEATHS >= __deaths_min__ AND TOTAL_DEATHS <= __deaths_max__) '
+                + '   OR (DEATHS >= __deaths_min__  AND DEATHS <= __deaths_max__)) ';
+
+    var quakes = alasql(query
+                .replace('__start_year__', years[0])
+                .replace('__end_year__', years[1])
+                .replace('__damage_min__', damage_descriptions[0])
+                .replace('__damage_max__', damage_descriptions[1])
+                .replace('__deaths_min__', deaths[0])
+                .replace('__deaths_max__', deaths[1]));
+
+    var new_plots = {};
+    for (i = 0; i < quakes.length; i++) {
+      quakes[i]['latitude'] = quakes[i]['LATITUDE'];
+      quakes[i]['longitude'] = quakes[i]['LONGITUDE'];
+      quakes[i]['tooltip'] = {content: EQS.make_html_tooltip(quakes[i])};
+      quakes[i]['size'] = EQS.ZOOM_SIZE;
+      if (quakes[i]['DAMAGE_DESCRIPTION'] == "4" 
+              || parseInt(quakes[i]['TOTAL_DEATHS']) > 10000
+              || parseInt(quakes[i]['DEATHS']) > 10000) {
+          quakes[i]['attrs'] = { fill: 'red' };
+      }
+      new_plots[quakes[i]['I_D']] = quakes[i];
+    }
+
+    $(".mapcontainer").trigger('update', [{
+        mapOptions: {'areas': {}, 'plots': {}}, 
+        newPlots: new_plots,
+        deletePlotKeys: Object.keys($(".mapcontainer").data('mapael').plots),
+        animDuration: 0 
+    }]);
+}
+
